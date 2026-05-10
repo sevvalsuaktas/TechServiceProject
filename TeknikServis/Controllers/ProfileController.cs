e0329b761bc3ed8f64efad7f73f9f66660bc55d7
sevvalsuaktas <sevvalsu.aktas@gazi.edu.tr>
2026-05-10 16:59:22 +0300

    Personellerin kendi bilgilerini güncelleyebilmesi için ProfileController ve Profilim sayfası eklendi

---
+++ TeknikServis/Controllers/ProfileController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TechService.DataAccess;
+using TechService.Models.Entities;
+
+namespace TechService.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProfileController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            // Login olurken sisteme kaydettiğimiz giriş biletinden (Claim) kullanıcının kendi ID'sini çekiyoruz
+            var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userIdStr)) return RedirectToAction("Index", "Login");
+
+            int userId = int.Parse(userIdStr);
+            var user = _context.Users.Find(userId);
+
+            if (user == null) return NotFound();
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(AppUser p)
+        {
+            var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            int userId = int.Parse(userIdStr);
+
+            var user = _context.Users.Find(userId);
+            if (user != null)
+            {
+                // Yeni seçtiği kullanıcı adı başka birisinde var mı? (Kendi ID'si hariç)
+                var exists = _context.Users.Any(x => x.Username == p.Username && x.Id != userId);
+                if (exists)
+                {
+                    ViewBag.ErrorMessage = "Bu kullanıcı adı zaten başka biri tarafından kullanılıyor!";
+                    return View(p);
+                }
+
+                // Bilgileri güncelle
+                user.Username = p.Username;
+                user.Password = p.Password;
+                _context.SaveChanges();
+
+                // Bilgiler değiştiği için tarayıcıdaki eski çerezin (Cookie) yenilenmesi lazım.
+                // Bu yüzden kullanıcıyı çıkış yapma (Logout) metoduna yönlendiriyoruz.
+                return RedirectToAction("Logout", "Login");
+            }
+
+            return View(p);
+        }
+    }
+}
